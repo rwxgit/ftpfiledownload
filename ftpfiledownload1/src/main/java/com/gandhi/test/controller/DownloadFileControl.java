174f290248efa0fb1d5803ef06eae4e3e365b807
rwxgit <gandhirajanselvaraj@gmail.com>
2018-09-19 23:07:32 +0530

    testing

--- ftpfiledownload1/src/main/java/com/gandhi/test/controller/DownloadFileControl.java
+++ ftpfiledownload1/src/main/java/com/gandhi/test/controller/DownloadFileControl.java
@@ -1,9 +1,9 @@
 package com.gandhi.test.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
+//import org.springframework.ui.ModelMap;
+//import org.springframework.validation.BindingResult;
+//import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
