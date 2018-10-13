c999725fef43182336bf285991c162b8bb741871
rwxgit <10249739+rwxgit@users.noreply.github.com>
2018-10-13 15:30:24 +0530

    Update DownloadFileService.java

--- ftpfiledownload1/src/main/java/com/gandhi/test/service/DownloadFileService.java
+++ ftpfiledownload1/src/main/java/com/gandhi/test/service/DownloadFileService.java
@@ -12,11 +12,12 @@ import org.apache.commons.net.ftp.FTPReply;
 public class DownloadFileService {
 
 	FTPClient ftp = null;
-	String server = "speedtest.tele2.net";
-	String username = "anonymous";
-	String password = "anonymous";
-	String remoteFilePath = "5MB.zip";
-	String localFilePath = "/media/gandhi/d_data/ftp/5MB.zip";
+	//String server = "11.138.64.52";
+	//String username = "selvgna";
+	//String password = "tars2222";
+	//String remoteFilePath = "\"" + "TTYA.SELVGNA.WEBSER.XGEN.WSDL5" + "\"";
+	//String remoteFilePath = "'" + "TTYA.SELVGNA.WEBSER.XGEN.WSDL5" + "'";
+	//String localFilePath = "C:\\ftp\\sample.txt";
 	
 	// constructor for DownloadFileService
     public DownloadFileService(String host, String user, String pwd) throws Exception {
@@ -28,12 +29,12 @@ public class DownloadFileService {
         int reply;
         
         try {
-			ftp.connect(server);
+			ftp.connect(host);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
         reply = ftp.getReplyCode();
-        System.out.println("Connected to " + server + "." + " replycode "
+        System.out.println("Connected to " + host + "." + " replycode "
         					+ reply);
         
         if (!FTPReply.isPositiveCompletion(reply)) {
@@ -41,17 +42,21 @@ public class DownloadFileService {
             throw new Exception("Exception in connecting to FTP Server");
         }
         
-        ftp.login(username, password);
-        ftp.setFileType(FTP.BINARY_FILE_TYPE);
-        ftp.enterLocalPassiveMode();
+        ftp.login(user, pwd);
+        //ftp.setFileType(FTP.BINARY_FILE_TYPE);
+        
+        ftp.setBufferSize(1048576);
+        ftp.setFileType(ftp.ASCII_FILE_TYPE);
+        
+        //ftp.enterLocalPassiveMode();
     }
 
     /* method to retrieve file from the remote system */
     public void downloadFile(String remoteFileP, String localFileP) {
         try {
-        	System.out.println("File to be downloaded is " + remoteFilePath);
-        	FileOutputStream fos = new FileOutputStream(localFilePath);
-        	this.ftp.retrieveFile(remoteFilePath, fos);
+        	System.out.println("File to be downloaded is " + remoteFileP);
+        	FileOutputStream fos = new FileOutputStream(localFileP);
+        	this.ftp.retrieveFile(remoteFileP, fos);
         } catch (IOException e) {
             e.printStackTrace();
         }
